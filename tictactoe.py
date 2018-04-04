6757df239b35110fbf6e5e32cb4d95ded01fc82c
samkrimmel <35492772+samkrimmel@users.noreply.github.com>
2018-04-04 08:36:01 -0400

    Updated from Brython Server: 4/4/2018 8:35:59 AM

--- tictactoe.py
+++ tictactoe.py
@@ -1,1 +1,47 @@
+#Sam Krimmel
+#4/4/18
+#tictactoe.py - graphics version
 
+#GRAPHICS
+
+    #colors
+
+black = Color(0x000000,1)
+white = Color(0xFFFFFF,1)
+red = Color(0xff0000,1)
+
+    #lines
+
+whiteLine = LineStyle(1,white)
+
+    #squares
+
+s1 = RectangleAsset(50,50, whiteLine, red)
+s2 = RectangleAsset
+s3 = RectangleAsset
+s4 = RectangleAsset
+s5 = RectangleAsset
+s6 = RectangleAsset
+s7 = RectangleAsset
+s8 = RectangleAsset
+s9 = RectangleAsset
+
+#imports
+
+from ggame import *
+
+#functions
+
+def printBoard():
+    
+def isEmpty(squareNumber):
+
+def winner():
+    
+def fullBoard():
+
+
+if __name__ == '__main__':
+    Sprite(s1)
+    App().run()
+    
